4f65a1d9a00c68e6628b4a68c148bb804b8015a8
Ravindra Jaju <jaju@msync.org>
2022-03-27 23:32:54 +0530

    Updates to dep versions. Clojure 1.11.0 too.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def version "0.1.0-SNAPSHOT")
-(def core-version "1.10.3")
-(def spring-version "5.3.15")
-(def spring-boot-version "2.6.3")
+(def core-version "1.11.0")
+(def spring-version "5.3.17")
+(def spring-boot-version "2.6.5")
 
 (defproject org.msync/spring-boost-classic version
 
