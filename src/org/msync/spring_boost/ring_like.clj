76fddaeb2ee43a2f8635b32308174748fdcf9627
Ravindra Jaju <jaju@msync.org>
2023-01-14 22:17:19 +0530

    Use inbuilt HttpServletRequest query-string getter

--- src/org/msync/spring_boost/ring_like.clj
+++ src/org/msync/spring_boost/ring_like.clj
@@ -1,41 +1,10 @@
 (ns org.msync.spring-boost.ring-like
   (:require [clojure.string])
-  (:import [org.springframework.util MultiValueMap]
-           [org.springframework.web.servlet.function ServerRequest]
+  (:import [org.springframework.web.servlet.function ServerRequest]
            [javax.servlet.http HttpServletRequest]
            [java.util Map Collections]))
 
-;; Reference - https://github.com/ring-clojure/ring/blob/master/SPEC
-
-(defn to-ring-headers [^Map spring-headers]
-  (persistent!
-    (reduce
-      (fn [acc [^String k ^String v]]
-        (assoc! acc (clojure.string/lower-case k) v))
-      (transient {})
-      spring-headers)))
-
-(defn to-query-string [^HttpServletRequest request]
-  (when-let [^MultiValueMap spring-query-params (.getParameterMap request)]
-    (clojure.string/join "&"
-      (flatten
-        (map
-          (fn [[k vs]]
-            (map #(str k "=" %) vs))
-          spring-query-params)))))
-
-(defn extract-headers [^HttpServletRequest http-request]
-  (let [header-keys (.getHeaderNames http-request)
-        header-keys (Collections/list header-keys)]
-    (persistent!
-      (reduce
-        (fn [m k]
-          (assoc! m k (.getHeader http-request k)))
-        (transient {})
-        header-keys))))
-
 (comment
-
   "Ref: https://github.com/ring-clojure/ring/blob/master/SPEC"
 
   "Ring request map contains the following key-value pairs."
@@ -63,6 +32,24 @@
    :body "Optional, ring.core.protocols/StreamableResponseBody. By default, the types that support this
    protocol are String (verbatim), ISeq (each element is sent as a string), File and InputStream (auto-closed)"})
 
+(defn to-ring-headers [^Map spring-headers]
+  (persistent!
+    (reduce
+      (fn [acc [^String k ^String v]]
+        (assoc! acc (clojure.string/lower-case k) v))
+      (transient {})
+      spring-headers)))
+
+(defn extract-headers [^HttpServletRequest http-request]
+  (let [header-keys (.getHeaderNames http-request)
+        header-keys (Collections/list header-keys)]
+    (persistent!
+      (reduce
+        (fn [m k]
+          (assoc! m k (.getHeader http-request k)))
+        (transient {})
+        header-keys))))
+
 (let [scheme :http
       http-protocol "HTTP/1.1"]
   (defn to-ring-spec
@@ -88,6 +75,6 @@
          :request-method request-method
          :protocol protocol
          :headers headers}
-        (when-let [query-string (to-query-string http-request)]
+        (when-let [query-string (.getQueryString http-request)]
           ;; Inefficient because spring-framework already parses it, but we re-create an approximation of the query-string
           {:query-string query-string})))))
